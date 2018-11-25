556f16109a1a9789995bdf0741cdafed86193d72
Rodrigo <rodrigo@zooft.company>
2018-11-25 16:46:44 -0300

    creating partials and helps in handlebars to enhace the website

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require('express');
+const hbs = require('hbs');
+const dateFormat = require('dateformat');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+var now = new Date();
 app.get('/', (req, res) => {
-  //res.send('<h1>Hello Express</h1>');
-  res.send({
-    name: 'Rodrigo',
-    last_name: 'Morgado',
-    hobbies: ['trail running', 'programming', 'family'],
-    age: 34
+  res.render('home.hbs', {
+    pageTitle: 'My first Website with Node',
+    welcomeMessage: 'Welcome to this fantastic web site',
+    currentTime: 'The time is ' + dateFormat(now)
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About this page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
