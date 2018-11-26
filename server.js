2946337fb444399dd3a0856bcf92fea155f7f19b
Rodrigo <rodrigo@zooft.company>
2018-11-26 16:32:05 -0300

    adding the page projects to show the portfolio of the company

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Everything falling'
